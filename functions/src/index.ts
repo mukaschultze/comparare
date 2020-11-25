65e51e76a142060aaa57bdb63dc33f8b1e6e6bdd
Muka Schultze <samuelschultze@gmail.com>
2020-11-25 13:29:45 -0300

    barcode endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,31 @@
-import * as functions from 'firebase-functions';
+import { Product } from "@comparare/types/core";
+import * as admin from "firebase-admin";
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// functions.logger.info("Hello logs!", {structuredData: true});
+
+const productConverter: FirebaseFirestore.FirestoreDataConverter<Product> = {
+  fromFirestore: (snapshot) => snapshot.data() as Product,
+  toFirestore: (data: Partial<Product>) => data,
+};
+
+const productsCollection = admin
+  .firestore()
+  .collection(`/produtos`)
+  .withConverter(productConverter);
+
+export const getProductFromBarcode = functions.https.onCall(
+  async (data, context) => {
+    const { barcode } = data;
+
+    if (typeof barcode !== "string") {
+      throw new Error(`Invalid barcode type "${typeof barcode}"`);
+    }
+
+    const query = productsCollection.where("barcode", "==", barcode);
+    const snapshot = await query.get();
+    const products = snapshot.docs.map((doc) => doc.data());
+
+    return products;
+  }
+);
