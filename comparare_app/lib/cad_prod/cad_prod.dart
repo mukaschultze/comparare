a93caa1e06729942ed4d3611aeb6586620bacd64
gustavokessler <gustavo.kessler@hotmail.com>
2021-01-20 13:44:20 -0300

    informações do produto ao selecionar

--- comparare_app/lib/cad_prod/cad_prod.dart
+++ comparare_app/lib/cad_prod/cad_prod.dart
@@ -1,4 +1,5 @@
 import 'package:comparare_app/models/mercado.model.dart';
+import 'package:comparare_app/models/productData.dart';
 import 'package:comparare_app/services/precos.service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -26,16 +27,21 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  var barcode = TextEditingController();
+  var name = TextEditingController();
+  var price = TextEditingController();
+  var unit = TextEditingController();
+  var quant = TextEditingController();
+
   List<Mercado> mercados = [];
   var service = new PrecosService();
-  var barcode = TextEditingController();
   List<String> nomes = [];
   Mercado selectedMercado;
   String currentText = "";
 
   String dropdownValue = 'Unidade';
   String _currentSelectedValue;
-  var _units = ["KG", "Unidade"];
+  var _units = ["Peso", "Unidade"];
 
   var mask = MoneyMaskedTextController(
       leftSymbol: "R\$ ", decimalSeparator: '.', thousandSeparator: ',');
@@ -44,6 +50,7 @@ class _BodyState extends State<Body> {
 
   bool searchingProduct = false;
   String searchingString = "Buscando produto...";
+  ProductData product = new ProductData();
 
   Future _scan() async {
     FlutterBarcodeScanner.scanBarcode(
@@ -99,32 +106,53 @@ class _BodyState extends State<Body> {
                       )),
                   keyboardType: TextInputType.emailAddress,
                 ),
-                onFocusChange: (hasFocus){
-                  if(!hasFocus && barcode.text != ""){
+                onFocusChange: (hasFocus) {
+                  if (!hasFocus &&
+                      barcode.text != "" &&
+                      barcode.text != product.barcode) {
                     setState(() {
                       searchingProduct = true;
                     });
-                      service.scanBarCode(barcode.text).then((value) => {
-                        if(value != null){
-                          setState((){
-                            searchingProduct = false;
-                          })
-                        }else{
-                          setState(() {
-                            searchingString = "Cadastre um novo produto";
-                          })
-                        }
-                      }).catchError((onError) => {
-                        searchingString = onError.message
-                      });
+                    service
+                        .scanBarCode(barcode.text)
+                        .then((value) => {
+                              if (value != null)
+                                {
+                                  setState(() {
+                                    searchingProduct = false;
+                                    product = ProductData.fromJson(value);
+                                    _currentSelectedValue = product.desc.unit;
+                                    if (selectedMercado != null &&
+                                        product.prices != null) {
+                                      var preco = product.prices
+                                          .firstWhere(
+                                              (element) =>
+                                                  element.mercadoId ==
+                                                  selectedMercado.id,
+                                              orElse: null)
+                                          .price;
+                                      mask.updateValue(
+                                          preco != null ? preco : 0.0);
+                                    }
+                                  })
+                                }
+                              else
+                                {
+                                  setState(() {
+                                    product = new ProductData();
+                                    searchingString =
+                                        "Cadastre um novo produto";
+                                    name.text = "";
+                                  })
+                                }
+                            })
+                        .catchError(
+                          (onError) => {searchingString = onError.message},
+                        );
                   }
                 },
               ),
-              SizedBox(
-                height: 6,
-              ),
-              if(searchingProduct)
-              Text(searchingString),
+              if (searchingProduct) Text(searchingString),
               SizedBox(
                 height: 6,
               ),
@@ -141,12 +169,12 @@ class _BodyState extends State<Body> {
                 textSubmitted: (text) {
                   selectedMercado =
                       mercados.firstWhere((element) => element.nome == text);
-                  print(selectedMercado.nome);
                 },
                 textChanged: (text) => currentText = text,
                 clearOnSubmit: false,
               ),
               TextField(
+                controller: name..text = product.nome,
                 decoration: InputDecoration(
                     labelText: "Nome do produto",
                     suffixIcon: IconButton(
@@ -185,11 +213,20 @@ class _BodyState extends State<Body> {
                 },
               ),
               TextField(
+                controller: quant
+                  ..text = product.desc != null ? product.desc.quant : "",
+                decoration: InputDecoration(
+                    labelText: "Quantidade",
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.add_shopping_cart),
+                    )),
+              ),
+              TextField(
                 controller: mask,
                 decoration: InputDecoration(
                     labelText: "Preço",
                     suffixIcon: IconButton(
-                      icon: Icon(Icons.credit_card),
+                      icon: Icon(Icons.monetization_on),
                     )),
               ),
               SizedBox(
@@ -197,7 +234,7 @@ class _BodyState extends State<Body> {
               ),
               RaisedButton(
                 onPressed: () => {},
-                child: Text("Cadastrar"),
+                child: Text("Salvar"),
                 color: Colors.blue,
                 textColor: Colors.white,
               ),
