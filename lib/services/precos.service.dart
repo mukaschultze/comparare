5ee686e7716ddee543e9f3edfbfca5676d02cb70
Muka Schultze <samuelschultze@gmail.com>
2021-01-29 16:22:51 -0300

    remove firebase functions

--- lib/services/precos.service.dart
+++ lib/services/precos.service.dart
@@ -1,19 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_functions/cloud_functions.dart';
 
 class PrecosService {
-  final HttpsCallable getProductFromBarcode =
-      FirebaseFunctions.instance.httpsCallable(
-    'getProductFromBarcode',
-  );
-
-  // Future<dynamic> scanBarCode(String code) async {
-  //   var result = await getProductFromBarcode.call(<String, dynamic>{
-  //     'barcode': code,
-  //   });
-  //   return result.data;
-  // }
-
   Stream<List<dynamic>> getMercados(List<String> ids) {
     return FirebaseFirestore.instance
         .collection('mercados')
